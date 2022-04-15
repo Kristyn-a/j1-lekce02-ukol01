3baa427180a08818b24001696657a85e0d084e1d
Kristýna <k.jozifova@gmail.com>
2022-04-15 14:15:29 +0200

    1. prasátko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,53 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
     }
 
+    private void nakresliPrasatko(Turtle zofka) {
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+
+        zofka.turnLeft(45);
+        zofka.move(42.4);
+        zofka.turnRight(90);
+        zofka.move(42.4);
+
+        zofka.penUp();
+        zofka.turnRight(135);
+        zofka.move(60);
+        zofka.penDown();
+
+        zofka.turnRight(30);
+        zofka.move(60);
+        zofka.turnLeft(180);
+        zofka.move(60);
+        zofka.turnRight(120);
+        zofka.move(60);
+
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(60);
+        zofka.turnRight(120);
+        zofka.move(100);
+        zofka.penDown();
+
+        zofka.turnRight(120);
+        zofka.move(60);
+        zofka.turnLeft(180);
+        zofka.move(60);
+        zofka.turnRight(120);
+        zofka.move(60);
+
+        nakresliPrasatko(zofka);
+    }
 }
+
+
