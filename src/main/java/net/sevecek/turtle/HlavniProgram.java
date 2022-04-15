01c6815d568551303c6c763781d9b33e82a8ed03
Kristýna <k.jozifova@gmail.com>
2022-04-15 22:44:14 +0200

    sluníčko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -9,62 +9,132 @@ public class HlavniProgram {
 
         zofka = new Turtle();
 
-        nakresliPrasatko(zofka);
-    }
-    private void nakresliPrasatko(Turtle zofka) {
+//        nakresliPrasatko(zofka);
+//    }
+//    private void nakresliPrasatko(Turtle zofka) {
+//
+//        zofka.move(60);
+//        zofka.turnRight(90);
+//        zofka.move(100);
+//        zofka.turnRight(90);
+//        zofka.move(60);
+//        zofka.turnRight(90);
+//        zofka.move(100);
+//        zofka.turnRight(90);
+//
+//        zofka.turnLeft(45);
+//        zofka.move(42.4);
+//        zofka.turnRight(90);
+//        zofka.move(42.4);
+//
+//        zofka.penUp();
+//        zofka.turnRight(135);
+//        zofka.move(60);
+//        zofka.penDown();
+//
+//        zofka.turnRight(30);
+//        zofka.move(60);
+//        zofka.turnLeft(180);
+//        zofka.move(60);
+//        zofka.turnRight(120);
+//        zofka.move(60);
+//
+//        zofka.penUp();
+//        zofka.turnLeft(180);
+//        zofka.move(60);
+//        zofka.turnRight(120);
+//        zofka.move(100);
+//        zofka.penDown();
+//
+//        zofka.turnRight(120);
+//        zofka.move(60);
+//        zofka.turnLeft(180);
+//        zofka.move(60);
+//        zofka.turnRight(120);
+//        zofka.move(60);
+//
+//        zofka.penUp();
+//        zofka.turnLeft(180);
+//        zofka.move(60);
+//        zofka.turnLeft(60);
+//        zofka.move(300);
+//        zofka.turnRight(90);
+//        zofka.penDown();
+//
 
-        zofka.move(60);
-        zofka.turnRight(90);
-        zofka.move(100);
-        zofka.turnRight(90);
-        zofka.move(60);
-        zofka.turnRight(90);
-        zofka.move(100);
-        zofka.turnRight(90);
-
-        zofka.turnLeft(45);
-        zofka.move(42.4);
-        zofka.turnRight(90);
-        zofka.move(42.4);
+//        zofka.penUp();
+//        zofka.turnLeft(90);
+//        zofka.move(400);
+//        zofka.turnRight(90);
+//        zofka.move(200);
+//        zofka.penDown();
 
         zofka.penUp();
-        zofka.turnRight(135);
-        zofka.move(60);
+        zofka.turnLeft(90);
+        zofka.move(400);
+        zofka.turnRight(90);
+        zofka.move(200);
         zofka.penDown();
 
+        for (int i = 0; i < 48; i++) {
+              zofka.move(2);
+              zofka.turnRight(7.5);
+        }
+
+        paprsekSlunicka(zofka);
+    }
+    private void paprsekSlunicka(Turtle zofka) {
+        zofka.turnLeft(90);
+        zofka.move(15);
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(15);
+        zofka.turnLeft(90);
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(2);
+            zofka.turnRight(7.5);
+        }
+
+        zofka.penDown();
+
+        paprsekSlunicka(zofka);
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(350);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.penDown();
+
+        postavenyDomecek(zofka);
+
+        mezeraUdomecku(zofka);
+    }
+
+    private void mezeraUdomecku(Turtle zofka) {
+        zofka.penUp();
         zofka.turnRight(30);
         zofka.move(60);
-        zofka.turnLeft(180);
-        zofka.move(60);
-        zofka.turnRight(120);
-        zofka.move(60);
-
-        zofka.penUp();
-        zofka.turnLeft(180);
-        zofka.move(60);
-        zofka.turnRight(120);
-        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(80);
         zofka.penDown();
+    }
 
-        zofka.turnRight(120);
-        zofka.move(60);
-        zofka.turnLeft(180);
-        zofka.move(60);
-        zofka.turnRight(120);
-        zofka.move(60);
-
-        zofka.penUp();
-        zofka.turnLeft(180);
-        zofka.move(60);
-        zofka.turnLeft(60);
-        zofka.move(300);
+    private void postavenyDomecek(Turtle zofka) {
         zofka.turnRight(90);
-        zofka.penDown();
+        zofka.move(40);
+        zofka.turnRight(90);
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(40);
+        zofka.turnRight(90);
+        zofka.move(60);
+        zofka.turnRight(30);
+        zofka.move(40);
+        zofka.turnRight(120);
+        zofka.move(40);
 
-        for (int i = 0; i < 2; i++) {
-            nakresliPrasatko(zofka);
-        }
     }
 }
-
 
